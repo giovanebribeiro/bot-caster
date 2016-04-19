a53a9a4d8ce084d6733dee36fc24c9a68e1aa5fa
Giovane Boaviagem <giovanebribeiro@gmail.com>
2016-04-19 16:36:19 -0300

    implementing the main module (memo)

--- test/memoTests.js
+++ test/memoTests.js
@@ -5,18 +5,32 @@
   var path = require('path');
   var cli = require('../cli.js');
 
-  describe("'memo' tests", function(){
+  describe("memo tests", function(){
     var testFile;
     before(function(done){
       testFile = path.join(__dirname,'testConfigs.js');
       done();
     });
 
-    it("cli test", function(done){
-      var memo = cli(testFile);
-      assert.ok(memo.slack);
-      done();
+    describe("'cli' module tests", function(){
+      it("file where 'memo' object is not present", function(done){
+        assert.throws(function(){
+          try{
+            cli(path.join(__dirname,'..','package.json'));
+          }catch(e){
+            throw e;
+          }
+        }, Error);
+        done();
+      });
+
+      it("correct file", function(done){
+        var memo = cli(testFile);
+        assert.ok(memo, "the 'memo' object must exist on this file.");
+        done();
+      });
     });
+
   });
 
 
