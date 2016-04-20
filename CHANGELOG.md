2cff973341312b7487f6fe4b5821d762f64889d7
Giovane Boaviagem <giovanebribeiro@gmail.com>
2016-04-19 22:16:36 -0300

    feat: implement the slack integration

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+<a name="0.0.1"></a>
+## 0.0.1 (2016-04-19)
+
+
+### Features
+
+* implement the client module, with tests. ([1206a9a](https://github.com/giovanebribeiro/memo/commit/1206a9a))
+
+
+
+<a name="0.0.0"></a>
+## 0.0.0 (2016-04-19)
+
+
+### Features
+
+* implement the client module, with tests. ([1206a9a](https://github.com/giovanebribeiro/memo/commit/1206a9a))
+
+
+
--- test/memoTests.js
+++ test/memoTests.js
@@ -3,7 +3,8 @@
 
   var assert = require('chai').assert;
   var path = require('path');
-  var cli = require('../cli.js');
+  var cli = require('../lib/cli.js');
+  var slackMemo = require('../lib/slack-memo.js');
 
   describe("memo tests", function(){
     var testFile;
@@ -12,26 +13,44 @@
       done();
     });
 
-    describe("'cli' module tests", function(){
-      it("file where 'memo' object is not present", function(done){
-        assert.throws(function(){
-          try{
-            cli(path.join(__dirname,'..','package.json'));
-          }catch(e){
-            throw e;
-          }
-        }, Error);
-        done();
-      });
-
-      it("correct file", function(done){
-        var memo = cli(testFile);
-        assert.ok(memo, "the 'memo' object must exist on this file.");
-        done();
-      });
+    it("file where 'memo' object is not present", function(done){
+      assert.throws(function(){
+        try{
+          cli(path.join(__dirname,'..','package.json'));
+        }catch(e){
+          throw e;
+        }
+      }, Error);
+      done();
     });
 
+    it("-> send the message to slack", function(done){
+      var msg = "*Release Version*";
+      msg += "\n\n";
+      msg += "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+      var configs = require(testFile);
+      assert.doesNotThrow(function(){
+        try{
+          slackMemo(configs.memo, msg, function(err, response){
+            if(err) throw err;
+            console.log("[slack] Message send successfully.");
+            done(); 
+          });
+        }catch(e){
+          throw e;
+        }
+      }, Error, "function does not throw any error");
+    });
+
+    
+/*
+    it(" -> lib test", function(done){
+      cli(testFile, "key1");
+      //assert.ok(memo, "the 'memo' object must exist on this file.");
+      done();
+    });*/
   });
+
 
 
 })();
